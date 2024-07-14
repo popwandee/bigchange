8b174fb3f20ef520880f89892442d87523c0c68b
popwandee <popwandee@gmail.com>
2024-07-14 22:19:11 +0700

    delete project

--- stock_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Stock
-# Register your models here.
-
-
-@admin.register(Stock)
-class StockAdmin(admin.ModelAdmin):
-    list_display = ('name', 'ev','ebitda','ev_ebitda', 'detail')  # Customize as needed
-
--- stock_app/forms.py
+++
@@ -1,9 +0,0 @@
-# myapp/forms.py
-
-from django import forms
-from .models import Stock
-
-class StockForm(forms.ModelForm):
-    class Meta:
-        model = Stock
-        fields = ['name', 'ev','ebitda','ev_ebitda', 'detail']  # Include all fields you want to use in the form
