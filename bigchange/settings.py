00c7b90d2dcbd69e16e5cc83c93de4c25ae90e8b
popwandee <popwandee@gmail.com>
2024-07-14 13:39:14 +0700

    allow host on bigchange onrender.com

--- bigchange/settings.py
+++ bigchange/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ekpg358&uf^x3!*^h%lp2f($x#a%jx=9!!-3sz#3ix=w6*_!zy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bigchange.onrender.com','localhost','127.0.0.1']
 
 
 # Application definition
