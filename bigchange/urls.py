b8e632fb385c1120831926d6092830bf5af93f6f
popwandee <popwandee@gmail.com>
2024-07-14 20:46:34 +0700

    edit admin url duplicate

--- bigchange/urls.py
+++ bigchange/urls.py
@@ -17,9 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include,path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
 # stock_project/urls.py
 
 urlpatterns = [
