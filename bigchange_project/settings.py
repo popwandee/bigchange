e79c804d1b73e8d606637da859a84be9476c1353
popwandee <popwandee@gmail.com>
2024-07-29 22:44:58 +0700

    Update settings.py

--- bigchange_project/settings.py
+++ bigchange_project/settings.py
@@ -86,11 +86,11 @@ else:
     DATABASE_URL = os.getenv('EXTERNAL_DATABASE_URL')
 
 DATABASES = {
-    'sqlite': {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     },
-    'default': dj_database_url.parse(DATABASE_URL)
+    'render': dj_database_url.parse(DATABASE_URL)
 }
 
 
