8acc2d6ad417262df47711a79f7e61b54e2ac233
popwandee <popwandee@gmail.com>
2024-07-29 22:59:24 +0700

    Update settings.py

--- bigchange_project/settings.py
+++ bigchange_project/settings.py
@@ -89,8 +89,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    'render': dj_database_url.parse(DATABASE_URL)
+    }
+    #,
+    #'render': dj_database_url.parse(DATABASE_URL)
 }
 
 
