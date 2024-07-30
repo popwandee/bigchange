9de609f431ee379a9fc5a1f1024b8a9a42f5f74f
popwandee <popwandee@gmail.com>
2024-07-30 09:20:10 +0700

    Update settings.py update BASE_DIR

--- bigchange_project/settings.py
+++ bigchange_project/settings.py
@@ -16,7 +16,8 @@ import dj_database_url
 #from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-#BASE_DIR = '/' #Path(__file__).resolve().parent.parent
+#BASE_DIR = #Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 #load_dotenv()  # This will load the environment variables from the .env file
 
